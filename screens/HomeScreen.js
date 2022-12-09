6ec7d92e547b02c0312406277485f0ae1e95e915
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-12-09 10:32:40 +0100

    Header finished

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,65 @@
-import { Text, View, Image } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useLayoutEffect } from 'react';
+import { Text, View, Image, TextInput, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/core'
-// import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from 'react-native-heroicons/outline'
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerShown: false
+      headerShown: false,
     })
   }, [])
 
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className='bg-white pt-5'>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+        />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl items-center'>
+            Current Location{' '}
+            <ChevronDownIcon
+              size={20}
+              color='#00CCBB'
+            />
+          </Text>
+        </View>
+
+        <UserIcon
+          size={35}
+          color='#00CCBB'
+        />
+      </View>
+
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3 items-center'>
+          <MagnifyingGlassIcon
+            size={20}
+            color='gray'
+          />
+          <TextInput
+            placeholder='Restaraunts and cuisines'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsVerticalIcon color='#00CCBB' />
+      </View>
+
+      <ScrollView>
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   )
 }
